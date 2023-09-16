bf40216a5ed169ade0e193cf82d7268b33d0d914
Gustavo Campos <115758013+gugacmp@users.noreply.github.com>
2023-09-16 11:17:29 -0300

    Add files via upload

---
+++ harmonica.py
@@ -0,0 +1,20 @@
+print('Campos Harmonicos')
+
+G = ['G' ,'Am', 'Bm', 'C', 'D', 'Em']
+A = ['A', 'Bm', 'Cm', 'D', 'E', 'Fm']
+B = ['B', 'Cm', 'Dm', 'E', 'F', 'Gm']
+C = ['C', 'Dm', 'Em', 'F', 'G', 'Am']
+D = ['D', 'Em', 'Fm', 'G', 'A', 'Bm']
+E = ['E', 'Fm', 'Gm', 'A', 'B', 'Cm']
+F = ['F', 'Gm', 'Am', 'B', 'C', 'Dm']
+
+#1 5 6 4
+
+print(f'Musica Campo Harmonico em G :', G[0], G[4], G[5], G[3])
+print(f'Musica Campo Harmonico em A :', A[0], A[4], A[5], A[3])
+print(f'Musica Campo Harmonico em B :', B[0], B[4], B[5], B[3])
+print(f'Musica Campo Harmonico em C :', C[0], C[4], C[5], C[3])
+print(f'Musica Campo Harmonico em D :', D[0], D[4], D[5], D[3])
+print(f'Musica Campo Harmonico em E :', E[0], E[4], E[5], E[3])
+print(f'Musica Campo Harmonico em F :', F[0], F[4], F[5], F[3])
+
